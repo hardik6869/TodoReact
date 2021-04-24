a9ea219507e9c3f884776ea0f0d9d59682b9ab3e
Hardik <sorthiyahardik6869@gmail.com>
2021-04-24 16:03:18 +0530

    state

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -5,6 +5,10 @@ const ToDoList = ()=>{
     const[inputList,setInputList] = useState("");
     const[Items, setItems] = useState([]);
 
+    const [number, setNumber] = useState(0);
+    let num2 = 0;
+    
+
     const itemEvent = (event) => {
         setInputList(event.target.value);
     };
@@ -15,7 +19,21 @@ const ToDoList = ()=>{
         });
         setInputList("");
     };
-
+    const deleteItem = (id) => {
+        let tmpItems = Items;
+        // console.log(tmpItems.splice(id,1));
+        tmpItems.splice(id,1);
+        setItems(tmpItems);
+    }
+    const inc = () =>{
+        let tmp = number; 
+        tmp++; 
+        setNumber(tmp);
+    }
+    const dec = () =>{
+        setNumber(number-1);
+    } 
+    
     return (<>
     <div className="main_div">
     <div className="center_div">
@@ -28,7 +46,8 @@ const ToDoList = ()=>{
        { Items.map( (itemval,index)=>{
                 return (<>
                 <div className="todo_style">
-                <i className="fas fa-times" aria-hidden="true" /><li  key={index} id={index} >{itemval}</li>
+                <i className="fas fa-times" aria-hidden="true" /><li  key={index} id={index} >{itemval}</li>\
+                <button onClick={() => deleteItem(index)}> delete </button>
                 </div>
                 </>
                 );
@@ -36,6 +55,18 @@ const ToDoList = ()=>{
        }    
        </ol>
     </div>
+    </div>
+    <div>
+        <center>    
+            <h1> {number} </h1> <br/>
+            <button onClick={inc}> + </button>
+            <button onClick={dec}> - </button>
+        </center>
+        <center>    
+            <h1> {num2} </h1> <br/>
+            <button onClick={() => {num2++; console.log(num2)}}> + </button>
+            <button onClick={() => {num2--; console.log(num2)}}> - </button>
+        </center>
     </div>
     </>);
 };
